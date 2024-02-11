7f329da0b44bea2b7a7aa43492d78a1d76ff9df0
Ravindran8499 <ravindransri01@gmail.com>
2024-02-11 19:28:44 +0530

    Created Navbar and modified Search Component

---
+++ src/components/Favourites.jsx
@@ -0,0 +1,5 @@
+const Favourites = () => {
+  return <div>This is from Favourites page</div>;
+};
+
+export default Favourites;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="px-5 flex justify-between items-center py-8  mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+      <NavLink to={"/"}>Food Recepie</NavLink>
+      <form className="flex gap-3">
+        <input
+          type="text"
+          placeholder="Search here..."
+          name="search"
+          className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
+        />
+        <button type="button">Search</button>
+      </form>
+      <ul className="flex flex-row gap-8">
+        <li>
+          <NavLink to={"/"}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/favourites"}>Favourite</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
